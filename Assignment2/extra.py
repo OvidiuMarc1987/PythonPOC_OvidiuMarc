f643a1d9c167505b451d0630319967a41ddafb8c
Ovidiu <ovidiu.marc@hotmail.com>
2021-05-07 21:29:17 +0300

    2nd task of extra assignment

--- Assignment2/extra.py
+++ Assignment2/extra.py
@@ -21,5 +21,15 @@ def remove_element_from_tuple():
     print(f"Type: {str(type(tuple_elements))}, Content: {tuple_elements}")
 
 
+def change_last_item_list():
+    original_list = [1, 2, 3, 4, "almost", "final"]
+    print(f"Original list: {original_list}")
+    original_list[-1] = "last"
+    print(f"Edited list {original_list}")
+
+
 if __name__ == "__main__":
+    # 1st task
     remove_element_from_tuple()
+    # 2nd task
+    change_last_item_list()
